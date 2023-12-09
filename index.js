4dffaf8d000378dedcbac353776330da4519e5da
FARHAAN KHAN <farhaan.khan@city.ac.uk>
2023-12-09 17:34:46 +0000

    edited

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import { express } from 'expnress';
 import path from 'path'
+// var fileupload = require("express-fileupload");
+// import express
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.sendFile("index.html", {root: path.join("./")})
+  res.sendFile("index.html", {root: path.join(__dirname)})
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port http//localhost:${port}`)
 })
+
